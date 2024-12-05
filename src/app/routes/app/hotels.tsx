9465f3f0725d6c7ed97919b359c146ce4485b791
Georgi Fotev <gfotev@Georgis-MacBook-Pro.local>
2024-12-05 15:27:54 +0200

    Adjusted table component to accept dynamic data

--- src/app/routes/app/hotels.tsx
+++ src/app/routes/app/hotels.tsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
 import { Table } from "../../../components/ui/table/table";
 
+type HotelsData = {
+    hotel_id: string;
+    name: string;
+    address: string;
+};
+
 export const HotelsRoute = () => {
-  return (
-    <>
-      <Table />
-    </>
-  );
+    const [data, setData] = useState<HotelsData[]>([]);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/v1/hotels")
+            .then((res) => {
+                return res.json();
+            })
+            .then((res) => {
+                console.log(res);
+                setData(res.data);
+            });
+    }, []);
+    return (
+        <>
+            <Table<Omit<HotelsData, "hotel_id">>
+                data={data}
+                columns={{
+                    hotelName: {
+                        name: "Name",
+                        accessor: (data) => data.name,
+                    },
+                    hotelAddress: {
+                        name: "Adress",
+                        accessor: (data) => data.address,
+                    },
+                }}
+            />
+        </>
+    );
 };
