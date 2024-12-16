4c545bef93033e786bcd4e91af0e6ffa18f52682
Georgi Fotev <georgifotev@protonmail.com>
2024-12-16 19:14:58 +0200

    on register success update

--- src/app/routes/auth/register.tsx
+++ src/app/routes/auth/register.tsx
@@ -1,5 +1,7 @@
+import { Check } from "lucide-react";
 import { Form } from "../../../components/ui/form/form";
 import { AuthFooter } from "../../../components/ui/form/form-footer";
+import { Link } from "../../../components/ui/link/link";
 import { paths } from "../../../config/paths";
 import { useRegister } from "../../../lib/auth";
 import { registerFields } from "./form-content";
@@ -10,20 +12,40 @@ export const RegisterRoute = () => {
   return (
     <div className="w-full h-screen flex">
       <div className="m-auto w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <Form
-          title="Register to our platform"
-          fields={registerFields}
-          onSubmit={(data) => register.mutate(data)}
-          submitButtonText="Create account"
-        >
-          {
-            <AuthFooter
-              path={paths.auth.login.path}
-              text="Already have an account?"
-              linkText="Sign In"
+        {register.isSuccess ? (
+          <div className="flex flex-col items-center gap-3">
+            <Check
+              size={48}
+              color="green"
+              className="border-4 border-green-500 rounded-full"
             />
-          }
-        </Form>
+            <h2 className="text-center text-lg font-bold">
+              Your submission was successful and your profile will be reviewed
+              shortly
+            </h2>
+            <Link
+              to={paths.auth.login.path}
+              className="text-lg text-blue-700 hover:underline dark:text-blue-500"
+            >
+              Log In
+            </Link>
+          </div>
+        ) : (
+          <Form
+            title="Register to our platform"
+            fields={registerFields}
+            onSubmit={(data) => register.mutate(data)}
+            submitButtonText="Create account"
+          >
+            {
+              <AuthFooter
+                path={paths.auth.login.path}
+                text="Already have an account?"
+                linkText="Sign In"
+              />
+            }
+          </Form>
+        )}
       </div>
     </div>
   );
