9eda7eb833b3561050652d79d1278d9aa2e666e4
Georgi Fotev <georgifotev@protonmail.com>
2025-01-10 06:10:40 +0200

    display hotel name in dashboard

--- src/app/routes/app/dashboard.tsx
+++ src/app/routes/app/dashboard.tsx
@@ -1,11 +1,18 @@
+import { useSelectedHotelStore } from "../../../store/selected-hotel-store";
+
 export const DashboardRoute = () => {
+    const { selectedHotel } = useSelectedHotelStore();
     return (
         <>
+            <div className="mb-8">
+                <h1>{selectedHotel.name}</h1>
+                <p className="text-xl">{selectedHotel.address}</p>
+            </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
-                <div className="border-2 border-dashed border-gray-300 rounded-lg dark:border-gray-600 h-32 md:h-64"></div>
-                <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"></div>
-                <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"></div>
-                <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"></div>
+                <div className="border-2 border-gray-300 rounded-lg dark:border-gray-600 h-32"></div>
+                <div className="border-2 rounded-lg border-gray-300 dark:border-gray-600 h-32"></div>
+                <div className="border-2 rounded-lg border-gray-300 dark:border-gray-600 h-32"></div>
+                <div className="border-2 rounded-lg border-gray-300 dark:border-gray-600 h-32"></div>
             </div>
             <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4"></div>
             <div className="grid grid-cols-2 gap-4 mb-4">
